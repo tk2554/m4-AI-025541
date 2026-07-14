8adb34612b0af7f497f21653670df62eefed7e21
35SprouTS <99402928+tk2554@users.noreply.github.com>
2026-07-14 14:51:29 +0700

    Add files via upload

--- 025541_w6.py
+++ 025541_w6.py
@@ -1,16 +1,20 @@
 target = 6700000
 bank = 0
 days = 1
-while bank < target or days > 30:
-    days += 1
-    while True:
-        try:
-            bank = float(str(input(f"Enter the deposit amount for day {days}: ")))
-            break
-        except:
-            print("Error")
-    print(f"Bank: {bank} Baht")
-if bank >= target:
-    print(f"บรรลุเลุป้าป้หมายการออมแล้ว")
+for days in range(1, 31):
+	while True:
+		try:
+			desposit = float(str(input(f"Enter the deposit amount for day {days}: ")))
+			if desposit == 0:
+				print("วันนี้ไม่ได้ออมเงิน ข้ามไปยังวันถัดไป")
+				continue
+			bank += desposit
+			break
+		except:
+			print("Error")
+	print(f"Bank: {bank} Baht")
+	if bank >= target:
+		print(f"บรรลุเลุป้าป้หมายการออมแล้ว")
+		break
 else:
-    print(f"ไม่สามารถบรรลุเป้าหมายการออมได้")
+		print(f"ไม่สามารถบรรลุเป้าหมายการออมได้")
